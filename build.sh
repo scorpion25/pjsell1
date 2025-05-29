809cabcbae29db76feb70a918043a2a324d6e46e
Anderson Silva <s.ct@live.com>
2025-05-29 13:37:18 -0300

    Criando superusuário no build

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 
 # Aplica as migrações
 python manage.py migrate --noinput
+
+echo "👤 Criando superusuário..."
+
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '121181')
+EOF
